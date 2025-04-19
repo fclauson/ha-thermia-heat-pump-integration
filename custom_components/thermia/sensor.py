39307629d52063b8aaaba22680eda2539555fdcb
Francis Clauson <8816936+fclauson@users.noreply.github.com>
2025-04-19 12:22:15 +0100

    Update sensor.py

--- custom_components/thermia/sensor.py
+++ custom_components/thermia/sensor.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import UnitOfTemperature, UnitOfTime
+from homeassistant.const import UnitOfTemperature, UnitOfTime, UnitOfPressure 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -455,7 +455,7 @@ async def async_setup_entry(
                     None,
                     "measurement",
                     "evaporator_pressure",
-                    UnitOfTemperature.ATMOSPHERIC_PRESSURE,
+                    UnitOfPressure.BAR, 
                 )
             )
 
