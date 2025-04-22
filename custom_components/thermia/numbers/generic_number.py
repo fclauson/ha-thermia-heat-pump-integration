369dede107a9b89f74e1ec00b471d5bb7a3320a3
Francis Clauson <8816936+fclauson@users.noreply.github.com>
2025-04-22 18:58:14 +0100

    Update generic_number.py

--- custom_components/thermia/numbers/generic_number.py
+++ custom_components/thermia/numbers/generic_number.py
@@ -5,9 +5,8 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.components.number import NumberEntity, NumberDeviceClass
 
-# from .const import DOMAIN
-# from .coordinator import ThermiaDataUpdateCoordinator
-
+from ..const import DOMAIN
+from ..coordinator import ThermiaDataUpdateCoordinator
 
 class ThermiaGenericSensor(
     CoordinatorEntity[ThermiaDataUpdateCoordinator], NumberEntity
