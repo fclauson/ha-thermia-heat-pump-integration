742f2b72e053de03528904cd95c9dc3f5ba60de5
Francis Clauson <8816936+fclauson@users.noreply.github.com>
2025-04-24 12:14:49 +0100

    Add files via upload

--- custom_components/thermia/numbers/generic_number.py
+++ custom_components/thermia/numbers/generic_number.py
@@ -1,103 +1,102 @@
+"""Thermia Generic number integration."""
+
 from __future__ import annotations
 
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.number import NumberEntity, NumberDeviceClass
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
 
 from ..const import DOMAIN
 from ..coordinator import ThermiaDataUpdateCoordinator
 
-class ThermiaGenericNumberEntity(
+class ThermiaGenericNumber(
     CoordinatorEntity[ThermiaDataUpdateCoordinator], NumberEntity
 ):   
     """Represents a generic number entity for Home Assistant."""
 
-    def __init__(self, name, coordinator, unique_id, initial_value=0, min_value=None, max_value=None, step=None, unit_of_measurement=None, device_class=None):
+    def __init__(
+        self, 
+        coordinator, 
+        idx,
+        is_online_prop : str,
+        number_name: str,
+        mdi_icon : str, 
+        entity_category: str,
+        device_class: str | None,
+        state_class : str, 
+        value_prop: str,
+        unit_of_measurement=None,
+          
+    ):
         """
         Initializes a new GenericNumberEntity.
-
-        Args:
-            name (str): The friendly name of the entity.
-            coordinator 
-            unique_id (str): A unique identifier for the entity.
-            initial_value (float, optional): The initial value of the number. Defaults to 0.
-            min_value (float, optional): The minimum allowed value. Defaults to None.
-            max_value (float, optional): The maximum allowed value. Defaults to None.
-            step (float, optional): The step increment for the number. Defaults to None.
-            unit_of_measurement (str, optional): The unit of measurement. Defaults to None.
-            device_class (str, optional): The device class of the number entity (e.g., temperature, humidity). Defaults to None.
         """
         super().__init__(coordinator)
         self.idx: int = idx
         
-        self._name = name
-        self._unique_id = unique_id
-        self._state = initial_value
-        self._min_value = min_value
-        self._max_value = max_value
-        self._step = step
-        self._unit_of_measurement = unit_of_measurement
-        self._device_class = device_class
-        self._callbacks = []  # List to store callbacks for state changes
+        self._is_online_prop: str = is_online_prop
+        self._number_name: str = number_name
+        self._mdi_icon: str = mdi_icon
+        self._entity_category: str = entity_category
+        self._device_class: str | None = device_class
+        self._state_class: str = state_class
+        self._value_prop: str = value_prop
+        self._unit_of_measurement: str | None = unit_of_measurement
+        # self.step: float step | None = step
+
+        
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return getattr(self.coordinator.data.heat_pumps[self.idx], self._is_online_prop)
 
     @property
     def name(self):
-        """Return the friendly name of the entity."""
-        return self._name
+        """Return the name of the number."""
+        return f"{self.coordinator.data.heat_pumps[self.idx].name} {self._number_name}"
 
     @property
     def unique_id(self):
-        """Return the unique ID of the entity."""
-        return self._unique_id
+        """Return the unique ID of the number."""
+        return f"{self.coordinator.data.heat_pumps[self.idx].name}_{self._number_name.lower().replace(' ', '_')}"
 
     @property
-    def state(self):
-        """Return the current state of the entity."""
-        return self._state
+    def icon(self):
+        """Return the icon of the number."""
+        return self._mdi_icon
 
     @property
-    def min_value(self):
-        """Return the minimum value of the number."""
-        return self._min_value
+    def device_info(self):
+        """Return device information."""
+        return {
+            "identifiers": {(DOMAIN, self.coordinator.data.heat_pumps[self.idx].id)},
+            "name": self.coordinator.data.heat_pumps[self.idx].name,
+            "manufacturer": "Thermia",
+            "model": self.coordinator.data.heat_pumps[self.idx].model,
+            "model_id": self.coordinator.data.heat_pumps[self.idx].model_id,
+        }
 
     @property
-    def max_value(self):
-        """Return the maximum value of the number."""
-        return self._max_value
-
-    @property
-    def step(self):
-        """Return the step increment of the number."""
-        return self._step
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of the entity."""
-        return self._unit_of_measurement
+    def entity_category(self):
+        """Return the category of the number."""
+        return self._entity_category
 
     @property
     def device_class(self):
-        """Return the device class of the entity."""
+        """Return the device class of the number."""
         return self._device_class
 
-    def set_value(self, new_value):
-        """Set the new value of the entity and notify listeners."""
-        try:
-            new_value = float(new_value)
-            if self._min_value is not None and new_value < self._min_value:
-                self._state = self._min_value
-            elif self._max_value is not None and new_value > self._max_value:
-                self._state = self._max_value
-            else:
-                self._state = new_value
-            self._notify_callbacks()
-        except ValueError:
-            print(f"Invalid value: {new_value}. Must be a number.")
+    @property
+    def state_class(self):
+        """Return the state class of the number."""
+        return self._state_class
 
-    def register_callback(self, callback):
-        """Register a callback function to be called when the state changes."""
-        self._callbacks.append(callback)
+    @property
+    def native_value(self):
+        """Return value of the number."""
+        return getattr(self.coordinator.data.heat_pumps[self.idx], self._value_prop)
 
-    def _notify_callbacks(self):
-        """Notify all registered callback functions about the state change."""
-        for callback in self._callbacks:
-            callback(self._state)
+    @property
+    def native_unit_of_measurement(self):
+        """Return the unit of measurement of the number."""
+        return self._unit_of_measurement
