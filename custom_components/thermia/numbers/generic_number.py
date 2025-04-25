5cc3be1c2730f49ada01c2a0f5d5d99602ff5433
Francis Clauson <8816936+fclauson@users.noreply.github.com>
2025-04-25 10:52:33 +0100

    Update generic_number.py

--- custom_components/thermia/numbers/generic_number.py
+++ custom_components/thermia/numbers/generic_number.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import logging
 
-from homeassistant.components.number import Number, NumberDeviceClass
+from homeassistant.components.number import NumberEintity 
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 
